de43cae0699a92b5375da09d43ac8a04cd1f979e
username <fruttys@gmail.com>
2022-04-27 21:59:38 +0300

    add changes

---
+++ paskaita/pratimai.js
@@ -0,0 +1,37 @@
+
+let weight=60;
+let growth=170;
+const myCity ="Vilnius";
+const myHairColor="brown";
+
+const birthDate=1806;
+const deathDate=1890;
+let myEyesColor="green";
+
+age = deathDate - birthDate;
+
+console.log(age);
+
+let isPet=true;
+let isSweet=false;
+let isRed=true;
+
+console.log(weight);
+console.log(growth);
+
+let user = "Matai";
+let goodbye = "Viso gero,";
+let hi = "Lauksim sugriztant!";
+
+result= goodbye + " " + user + " "+ hi;
+
+console.log(result);
+
+const yes=true;
+const no= false;
+
+console.log(15==15);
+console.log(24!=26);
+console.log(18===17);
+console.log(16!==15)
+
--- paskaita/script.js
+++ paskaita/script.js
@@ -8,7 +8,55 @@ Multi line komentaras
 // Sukuriam kintamuosius
 let myName;
 let age;
+let mySurname
+let result
+let average
+let percent
+
+// console.log(myName) // undefined - kai kintamasis yra sukurtas, bet neturi priskirtos reiksmes
+
 
 // Priskiriam kintamiejam reikšmes
-myName = "Liudmila";
-age = 19;
+myName = "Liudmila"; // string - zodis tarp kabuciu
+mySurname ="Liuliu"
+fullName = myName + " " + mySurname; // sudetis - tarp tokiu paciu tipu
+age = 19; // number - skaicius (gali buti sveikas, arba su desimtosiomis)
+result = age + age + age - 9; // 19+19+19 - 9=38
+result = result + result; // 38+38 = 96
+average = (8+8+9+7+9+10)/6; // 51/6 = 8.5
+// is viso surinkta 60 eu, as atnesiau 20 eu
+percent = 100*20/60;
+
+// console.log(myName)
+
+// console.log("9"+ "9"); //99
+// console.log(9+9); //18
+// console.log("760" + 40); // 76040
+// console.log(greetings);
+//sukuriam kintamuosius su const
+
+const myDogName="Kleika"
+const myDogAge=15;
+const myDoubleDogName = myDogName + myDogName;
+const greetings = "Sveikas" + " " + myDogName + " ";
+
+// 1. naudoti let tik tada kai reiksme yra kintanti, kitais atvejais naudoti const
+// 2. sukurus kintamaji visada priskiriam pradine reiksme
+// 3. Visada naudojame tryguba lygybe
+// visos neigiamos reiksmes yra undifined, null - neigiama reiksme, "" - neigiama reiksme, false
+
+
+// string, numbers, boolean (bool)
+
+const yes=true;
+const no = false;
+let isAdult = true;
+
+// console.log("Rokas"=="Rokas"); // rokas=Rokas = false
+// console.log(10!=10) //10 nelygu 10 = false
+// console.log(5 !=4) //5 nelygu 4 = true
+// console.log("5"==5) // true nes dviguba lygibe ne tikrina reiksmiu
+// console.log("5"===5) // false nes tryguba lygibe saugesne, nes patikrina ar reiksmes vienodo tipo
+
+console.log("4"!=4);
+console.log("4"!==4);
