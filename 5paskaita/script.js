a70bb8443ec7ecb322ea49c319c571d4c6f3278b
username <fruttys@gmail.com>
2022-05-04 20:38:59 +0300

    add index.html, id, class, DOM manipuliation

---
+++ 5paskaita/script.js
@@ -0,0 +1,28 @@
+console.log("5paskaita DOM manipuliavimas")
+
+const nameNode =document.getElementById("name");
+nameNode.textContent="Tomas";
+
+const descriptionNode = document.getElementById("description");
+descriptionNode.textContent = "Man patinka";
+
+document.body.style.backgroundColor = "f1f1f1";
+document.body.style.margin = 0;
+nameNode.style.color = "blue";
+nameNode.style.paddingLeft = "10px";
+descriptionNode.style.padding = "10px";
+descriptionNode.style.backgroundColor = "pink";
+
+// const discountNode = document.querySelector("li");
+// console.log(discountNode)
+
+// const liByTag = document.querySelector("li");
+// const liById = document.querySelector("#discount");
+// const liByClass = document.querySelector(".yellow");
+
+// const firstItem = document.querySelector("li:first-child");
+// const lastItem = document.querySelector("li:last-child");
+
+//  const discountedItems = document.querySelectorAll("#discount");
+//  console.log(discountedItems[0]);
+// console.log(discountedItems.length);
