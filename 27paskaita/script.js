a45f9cec7e5e0c2c7b457d332b902c37997b6940
username <fruttys@gmail.com>
2022-06-16 22:28:00 +0300

    add repeat practice from first

---
+++ 27paskaita/script.js
@@ -0,0 +1,69 @@
+const userName = 'Rokas';
+
+const goodbye = `Viso gero ${userName}, lauksime sugriztant`;
+// console.log(goodbye)
+
+const salary = 5000;
+ // console.log(`$${salary}`);
+
+ 
+//  const price = 10;
+  //if (price > 10) {
+     //console.log('perku')
+ //} else {
+   
+ //console.log('neperku')
+ //}
+
+ const price = 11;
+ const result = (price > 10) ? "Perku" : "Neperku";
+
+    //console.log(result);
+
+
+
+ const hours = 17
+
+ const resul = (hours>=0 && hours <=6) ? "naktis" : (hours>6 && hours<=12) ? "rytas" : (hours>12 && hours<=18) ? "diena" : (hours>18 && hours<=24) ? "vakaras" : "netinkamas skaicius";
+
+ //console.log (resul)
+
+
+ const myName = 'Liudmila';
+ const count = myName.length;
+ const firstChar = myName[0];
+ const lastChar = myName [myName.length -1]
+
+ for (let i=0; i < count; i++) {
+    const char = myName[i];
+    //console.log(char)
+    console.log(`${i} ${char}`)
+   
+ }
+     
+ console.log(firstChar);
+ console.log(lastChar);
+ console.log(count);
+ 
+ 
+
+
+
+
+
+
+ //let result = ''
+
+//  if  (hours>0 && hours<=6) {
+//      //console.log('naktis'); // result = "naktis";
+//  } else if (hours>6 && hours<=12) {
+//      //console.log('rytas')
+//  } else if (hours>12 && hours<=18) {
+//      //console.log('diena')
+//  } else if (hours>18 && hours<=24) {
+//     //console.log('vakaras')
+//  } else {
+//      //console.log('netinkamas laikas')
+//  }
+     
+ 
