dde2e87763639534728985b4ac6d0ad16c8683cf
username <fruttys@gmail.com>
2022-06-28 20:17:43 +0300

    add delete method

--- 25paskaita/script.js
+++ 25paskaita/script.js
@@ -1,88 +1,90 @@
 const renderTodo = (todo) => {
-    const {id, completed, title} = todo;
+  const { id, completed, title } = todo;
 
-    const todoEl = document.createElement('div');
-    const todoTitle = document.createElement('p');
-    const todoStatus = document.createElement('span');
+  const todoEl = document.createElement("div");
+  const todoTitle = document.createElement("p");
+  const todoActions = document.createElement("div");
+  const todoStatus = document.createElement("span");
+  const deleteButton = document.createElement("button");
 
-    todoEl.className ='todo';
-    todoTitle.className = 'todo-title';
-    todoStatus.className = completed ? 'todo-status done' : 'todo-status';
-    todoTitle.className = completed ? 'todo-title done' : 'todo-title' ;
+  todoEl.className = "todo";
+  todoTitle.className = "todo-title";
+  todoStatus.className = completed ? "todo-status done" : "todo-status";
+  todoActions.className = "todo-actions";
+  deleteButton.className = "todo-delete";
+  todoTitle.className = completed ? "todo-title done" : "todo-title";
 
+  todoTitle.textContent = title;
 
-    todoTitle.textContent = title;
+  deleteButton.textContent = "Delete";
 
-    todoStatus.addEventListener('click', () => {
-        
-        todoStatus.classList.toggle('done');
-        todoTitle.classList.toggle('done');
-        todoStatus.completed = !todoStatus.completed;
-        
-        console.log(completed);
-    })
-    
-    todoEl.append(todoTitle, todoStatus);
-    document.querySelector('.todo-container').prepend(todoEl);
+  todoStatus.addEventListener("click", () => {
+    todoStatus.classList.toggle("done");
+    todoTitle.classList.toggle("done");
+    todoStatus.completed = !todoStatus.completed;
+  });
 
-    
-};
-
-   
-
-fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(resp => resp.json())
-    .then((response) => {
-        const todos = response.slice(0, 5);
-        
-        todos.forEach((todo) => renderTodo(todo));
-    })
-    .catch((error) => {
-        console.error(error);
-    });
-
-
-   const handleAddTodo = () => {
-    const todoText = document.querySelector('input[name="todo-text')
-    
-    const newTodo = {title: todoText.value, completed: false};
-
+  deleteButton.addEventListener("click", () => {
     const params = {
-        method: 'POST',
-        body: JSON.stringify(newTodo),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-          }
+      method: "DELETE",
     };
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, params)
+      .then((resp) => resp.json())
+      .then((response) => {
+        console.log("successfully", todo);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+  todoActions.append(todoStatus, deleteButton);
 
-    fetch('https://jsonplaceholder.typicode.com/todos',params)
-    .then(resp => resp.json())
+  todoEl.append(todoTitle, todoActions);
+  document.querySelector(".todo-container").prepend(todoEl);
+};
+
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((resp) => resp.json())
+  .then((response) => {
+    const todos = response.slice(0, 5);
+
+    todos.forEach((todo) => renderTodo(todo));
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+const handleAddTodo = () => {
+  const todoText = document.querySelector('input[name="todo-text');
+
+  const newTodo = { title: todoText.value, completed: false };
+
+  const params = {
+    method: "POST",
+    body: JSON.stringify(newTodo),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  };
+
+  fetch("https://jsonplaceholder.typicode.com/todos", params)
+    .then((resp) => resp.json())
     .then((response) => {
-        renderTodo(response);
-        todoText.value = '';
-        console.log(response);
+      renderTodo(response);
+      todoText.value = "";
+      console.log(response);
     })
     .catch((error) => {
-        console.error(error)
+      console.error(error);
     });
-
 };
-    // renderTodo(newTodo);
-    // todoText.value = '';
+// renderTodo(newTodo);
+// todoText.value = '';
 
+const addButton = document.querySelector(".add-button");
+addButton.addEventListener("click", handleAddTodo);
 
-   const addButton= document.querySelector('.add-button');
-   addButton.addEventListener('click', handleAddTodo);
-
-
-
-
-
-
-
-
-
-    // Sukurti addEventListener su handlerliu onClick
+// Sukurti addEventListener su handlerliu onClick
 // Nuspaudus ant elemento pakeisti todoStatus klasę (jeigu klasė yra done, pakeisti į undone. Jeigu klasė yra undone, pakeisti į done)
 // Pakeisti todo status į priešinga, kaip su klase. jeigu buvo todo.completed = true, pakeisti i todo.completed = false;
 // Pakeisti teksto sviesuma/tamsuma kai yra done/undone
--- 27paskaita/dom.js
+++ 27paskaita/dom.js
@@ -276,7 +276,7 @@
 
 // Roko uzduotys
 
-const colors = ["red", "blue", "green", "white", "black", "yellow", "purple"];
+// const colors = ["red", "blue", "green", "white", "black", "yellow", "purple"];
 
 // function removeSomeColor(array) {
 //   return array.splice(2, 2);
@@ -291,10 +291,51 @@ const colors = ["red", "blue", "green", "white", "black", "yellow", "purple"];
 // orangeColor(colors);
 // console.log(colors);
 
-output = [];
-colors.forEach(function (el) {
-  if (/e/.test(el)) {
-    output.push(el);
-  }
-});
-console.log(output);
+// output = [];
+// colors.forEach(function (el) {
+//   if (/e/.test(el)) {
+//     output.push(el);
+//   }
+// });
+// console.log(output);
+
+const colors = ["red", "blue", "green", "white", "black", "yellow", "purple"];
+
+function removeGreenAndWhite(colorArray) {
+  const sliced = colorArray.slice(0, 2);
+  const sliced2 = colorArray.slice(-3);
+  const combine = sliced.concat(sliced2);
+  return combine;
+}
+
+const modifiedColors = removeGreenAndWhite(colors);
+
+function addOrange(colorArray) {
+  colorArray.splice(2, 1, "orange");
+}
+
+addOrange(modifiedColors);
+
+const hasEchar = modifiedColors.filter((color) => color.includes("e"));
+// "red".includes("e") => true
+// "blue".includes("e") => true
+console.log("hasEchar", hasEchar);
+
+const shorterThan4Chars = modifiedColors.filter((color) => color.length < 4);
+// "red".length < 4 => 3 < 4 => true
+// "blue".length < 4 => 4 < 4 => false
+// "orange".length < 4 => 6 < 4 => false
+console.log("shorter than 4 chars", shorterThan4Chars);
+
+const hasZChar = modifiedColors.some((color) => color.includes("z"));
+console.log("has Z char", hasZChar);
+// "red".includes("z") => false
+// "blue".includes("z") => false
+
+const hasBrownValue = modifiedColors.some((color) => color === "brown");
+console.log("has brown", hasBrownValue);
+
+const hasALetter = modifiedColors.filter((color) => color.includes("a"));
+const aLetterCount = hasALetter.length;
+
+console.log("has A letter", aLetterCount);
